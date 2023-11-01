b8d20764da7c6314693da5df78e0cfd566904caf
Jose Vasquez <josevasquez8365@gmail.com>
2023-11-01 18:43:50 -0400

    feat: add input validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-
-
-
 import { Map } from "./components/Map/Map";
 import { Loader } from "./components/Loader/Loader";
 import { Header } from "./components/Header/Header";
@@ -13,14 +10,17 @@ export const App = () => {
   const { info, status, getData } = useFetch({
     url: `https://geo.ipify.org/api/v2/country,city?apiKey=at_HXSruIikJaNhIb1aT7hvFc2TUzlp0&ipAddress=${ip}`,
   });
- const handleChangeInput = (e)=> {
-  setIp(e.target.value)
- }
- const searchLocation = ()=> {
-  getData()
-  setIp("")
-
- }
+  const handleChangeInput = (e) => {
+   
+    setIp(e.target.value);
+  };
+  const searchLocation = () => {
+    if(ip.trim() !== "") {
+      getData();
+      setIp("");
+    }
+   
+  };
 
   useEffect(() => {
     getData();
@@ -28,12 +28,27 @@ export const App = () => {
 
   return (
     <div className="container">
-      <Header info={info} status={status} searchLocation={searchLocation} handleChangeInput={handleChangeInput} ip={ip}/>
-      
-      {status=== REQUEST_STATUS.LOADING && <div className="loader-container"><Loader/></div> }
-      {status === REQUEST_STATUS.SUCCESS && <Map lat={info.location.lat} lng={info.location.lng} /> }
-      {status === REQUEST_STATUS.ERROR && <div className="loader-container"><h3>IP Address or domain not found, please provide a valid input.</h3></div>}
-      
+      <Header
+        info={info}
+        status={status}
+        searchLocation={searchLocation}
+        handleChangeInput={handleChangeInput}
+        ip={ip}
+      />
+
+      {status === REQUEST_STATUS.LOADING && (
+        <div className="loader-container">
+          <Loader />
+        </div>
+      )}
+      {status === REQUEST_STATUS.SUCCESS && (
+        <Map lat={info.location.lat} lng={info.location.lng} />
+      )}
+      {status === REQUEST_STATUS.ERROR && (
+        <div className="loader-container">
+          <h3>IP Address or domain not found, please provide a valid input.</h3>
+        </div>
+      )}
     </div>
   );
 };
